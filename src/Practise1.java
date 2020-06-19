8a3fa3936225f1787b4304327580ada3baff3e18
jagadeeshmohand <jagadeeshdm87@gmail.com>
2020-06-19 16:21:18 +0530

    StringHandling

---
+++ src/Practise1.java
@@ -0,0 +1,14 @@
+
+public class Practise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//payment $100 paid
+		String str ="payment $100 paid";
+        System.out.println(str.charAt(8));
+        String str1 ="payment $100 paid";
+       System.out.println( str1.indexOf("$"));
+       System.out.println(str1.substring(8));
+	}
+
+}
